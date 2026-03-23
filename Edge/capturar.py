c3c92a95d28cdd75a3f037e5fce341fcb54c1c3f
J-e-r-o <deleonjeronimo079@gmail.com>
2026-03-23 20:02:50 -0300

    La funcionalidad de sacar foto esta hecha

--- Edge/capturar.py
+++ Edge/capturar.py
@@ -1,25 +1,38 @@
 import cv2
+import os
+from datetime import datetime
 
-# Abrir la cámara (0 = cámara por defecto)
+# Obtener la ruta del script actual
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Ir a la carpeta "edge"
+carpeta = os.path.join(base_dir, "fotos")
+
+# Crear la carpeta si no existe
+os.makedirs(carpeta, exist_ok=True)
+
+# Nombre con fecha y hora
+nombre_foto = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".jpg"
+
+# Ruta completa
+ruta_foto = os.path.join(carpeta, nombre_foto)
+
+# Cámara
 cap = cv2.VideoCapture(0)
 
 if not cap.isOpened():
     print("No se pudo abrir la cámara")
     exit()
 
-# Capturar un frame
 ret, frame = cap.read()
 
 if ret:
-    # Guardar la imagen
-    cv2.imwrite("foto.png", frame)
-    print("Foto guardada como foto.png")
+    cv2.imwrite(ruta_foto, frame)
+    print(f"Foto guardada en: {ruta_foto}")
 else:
     print("No se pudo tomar la foto")
 
-# Liberar la cámara
 cap.release()
-
 
 
 
